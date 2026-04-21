d9b58fd897cb84986ba7d80ee12e711dd3ab4a03
sararojas3315656 <saresro04@gmail.com>
2026-04-21 11:00:10 -0600

    Ejercicio 5 Apropiación by Sara

--- JS/apropiacion/ejercicio_5.js
+++ JS/apropiacion/ejercicio_5.js
@@ -4,3 +4,14 @@ Cada elemento de nuevosDatos será un objeto parcial como {edad: 22} o {ciudad:
 "Medellín"}.
 • Combina todos los datos usando inmutabilidad y spread.
 Retorna el nuevo perfil completo.*/
+
+const perfil = { nombre: "Sara", edad: 18, ciudad: "Bucaramanga" };
+
+function actualizarPerfil(perfil, ...nuevosDatos) {
+    const actualizado = { ...perfil, ...nuevosDatos[0], ...nuevosDatos[1] };
+    console.log(actualizado);
+}
+
+actualizarPerfil(perfil, { edad: 22 }, { ciudad: "Medellín" });
+
+export { perfil, actualizarPerfil };
