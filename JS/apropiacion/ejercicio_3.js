efc4970452a6fa3c7487377c6454aa996d01a15c
sararojas3315656 <saresro04@gmail.com>
2026-04-21 10:44:33 -0600

    Ejercicio 3 apropiación realizado by Sara

--- JS/apropiacion/ejercicio_3.js
+++ JS/apropiacion/ejercicio_3.js
@@ -4,3 +4,14 @@ Escribe una función registrarActividad(actividad) que reciba un objeto que debe
 • Usa un try...catch para validar que ambas propiedades existan.
 • Si falta alguna, lanza un error indicando que la actividad no es válida.
 • Si todo está correcto, muestra un mensaje confirmando el registro.*/
+
+function registrarActividad(actividad) {
+    try {
+    const { nombre, fecha } = actividad;
+    console.log("Actividad registrada:", nombre, fecha);
+    } catch {
+    console.log("Error: La actividad no es válida");
+    }
+}
+
+export { registrarActividad };
